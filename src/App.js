61e2c81e1656bcaff415af87a8a291e076af3a5c
Tejamatti <tejmatti53@gmail.com>
2024-01-06 21:45:02 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Header from './Header';
 
-import ProductList from './ProductList';
+
 import Footer from './Footer';
 
 function App() {
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,93 +1,5 @@
-// // Header.js
-// import React, { useState, useEffect } from 'react';
-// import styled from 'styled-components';
-// import Categories from './Categories';
-// import ProductList from './ProductList';
 
-// const HeaderWrapper = styled.div`
-//   background-color: #2874f0;
-//   color: #fff;
-//   padding: 10px 20px;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-// `;
-
-// const Logo = styled.div`
-//   font-size: 24px;
-//   font-weight: bold;
-// `;
-
-// const SearchBar = styled.div`
-//   display: flex;
-
-//   input {
-//     padding: 5px;
-//     margin-right: 10px;
-//   }
-// `;
-
-// const Navigation = styled.nav`
-//   ul {
-//     list-style: none;
-//     display: flex;
-//   }
-
-//   li {
-//     margin-right: 20px;
-//   }
-
-//   a {
-//     text-decoration: none;
-//     color: #fff;
-//     &:hover {
-//       border-bottom: 2px solid #fff;
-//     }
-//   }
-// `;
-
-// const Header = () => {
-//   const [searchTerm, setSearchTerm] = useState('');
-
-//   const handleSearch = (event) => {
-//     setSearchTerm(event.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <HeaderWrapper>
-//         <Logo>Your Logo</Logo>
-//         <SearchBar>
-//           <input
-//             type="text"
-//             placeholder="Search products..."
-//             value={searchTerm}
-//             onChange={handleSearch}
-//           />
-//           <button onClick={() => setSearchTerm('')}>Clear</button>
-//         </SearchBar>
-//         <Navigation>
-//           <ul>
-//             <li><a href="#">Home</a></li>
-//             <li><a href="#">Categories</a></li>
-//             <li><a href="#">Deals</a></li>
-//             <li><a href="#">Account</a></li>
-//             <li><a href="#">Cart</a></li>
-//           </ul>
-//         </Navigation>
-//       </HeaderWrapper>
-//       <Categories />
-//       <ProductList searchTerm={searchTerm} />
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-
-// Header.js
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import styled from 'styled-components';
 import Categories from './Categories';
 import ProductList from './ProductList';
@@ -172,11 +84,11 @@ const Header = () => {
         </SearchBar>
         <Navigation>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Categories</a></li>
-            <li><a href="#">Deals</a></li>
-            <li><a href="#">Account</a></li>
-            <li><a href="#">Cart</a></li>
+            <li><a>Home</a></li>
+            <li><a>Categories</a></li>
+            <li><a>Deals</a></li>
+            <li><a>Account</a></li>
+            <li><a>Cart</a></li>
           </ul>
         </Navigation>
       </HeaderWrapper>
